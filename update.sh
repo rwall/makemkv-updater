b77d2e4f8ad6b5fcac6e4687772f4155f95fab6c
Rob Wall <rwall@markettrack.com>
2016-05-10 21:32:05 -0500

    confirmed working now. it didn't break my install this time

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 cleanup() {
 	cd /tmp
 	if [ -n "$TMPDIR" ]; then
-		rmdir -rf $TMPDIR
+		rm -rf $TMPDIR
 	fi
 }
 
@@ -19,11 +19,15 @@ MKVVER=''
 BINVER=''
 OSSVER=''
 if [ ! -f "$MKVCON" ]; then
-	echo "ERROR: can't find makemkvcon" 1>&2
-	exit 127
+	echo "MakeMKV not found. Overriding version number to force upgrade"
+	MKVVER='0.0.0'
 else
 	MKVVER=$($MKVCON version 2>/dev/null | head -n 1 | cut -d " " -f 2 | sed -e 's/^v//' )
-	echo "Found makemkvcon version: $MKVVER" 1>&2
+	if [ ! "$MKVVER" ]; then
+		MKVVER='0.0.0'
+		echo "MakeMKV but no version detected. Likely a half-broken install. Overriding the version number to force upgrade."
+	fi
+	echo "Using MakeMKV version: $MKVVER" 1>&2
 fi
 
 URL='http://www.makemkv.com/forum2/viewtopic.php?f=3&t=224'
@@ -63,7 +67,7 @@ else
 fi
 
 for ((i = 0; i <= 2; i++)); do
-	if [ ${BINVERASSOC[$i]} -lt ${INSTALLED_VERSION[$i]} ]; then
+	if [ "${BINVERASSOC[$i]}" -lt "${INSTALLED_VERSION[$i]}" ]; then
 		echo "Installed version is newer than bin/oss downloads" 1>&2
 		echo "(how does that even happen??)" 1>&2
 		exit 127
@@ -106,8 +110,6 @@ OSSFILE=${TMP[-1]}
 unset IFS
 BINDIR=$(basename $BINFILE .tar.gz)
 OSSDIR=$(basename $OSSFILE .tar.gz)
-echo "BIN file: $BINFILE"
-echo "OSS file: $OSSFILE"
 
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
@@ -116,10 +118,8 @@ GETCMD=
 CURL=$(which curl)
 WGET=$(which wget)
 if [ -f $CURL ]; then
-	echo "Found curl."
 	GETCMD="$CURL -O "
 elif [ -f $WGET ]; then
-	echo "Found wget."
 	GETCMD="$WGET "
 else
 	cleanup_and_bail 127 'Failed to find curl or wget. Make sure one is installed and in your PATH'
@@ -127,7 +127,7 @@ fi
 
 
 for type in bin oss; do
-	echo "Downloading file for $type"
+	echo "Downloading $type file"
 	$GETCMD ${URLASSOC[$type]} 2>/dev/null
 	if [ "$?" -ne "0" ]; then
 		cleanup_and_bail 126 "curl/wget returned failure status: $?"
@@ -144,6 +144,7 @@ tar zxf $BINFILE >/dev/null 2>&1
 if [ "$?" -ne "0" ]; then
 	cleanup_and_bail 124 "Failure extracting $BINFILE"
 fi
+echo "Extracting $OSSFILE"
 tar zxf $OSSFILE >/dev/null 2>&1
 if [ "$?" -ne "0" ]; then
 	cleanup_and_bail 124 "Failure extracting $OSSFILE"
@@ -153,15 +154,26 @@ fi
 apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev >/dev/null 2>&1 || cleanup_and_bail 123 "Failure installing necessary packages"
 
 # build oss part
+echo "Building OSS"
 cd $OSSDIR || cleanup_and_bail 122 "Failure to CD to $OSSDIR"
-(./configure && make && make install) >/dev/null 2>&1 || cleanup_and_bail 121 "OSS Build failure"
+(./configure && make) >/dev/null 2>&1		|| cleanup_and_bail 121 "OSS Build failure"
+cd $TMPDIR
 
 # build bin part
+echo "Building BIN"
 cd $BINDIR || cleanup_and_bail 122 "Failure to CD to $BINDIR"
-( make && make install) >/dev/null 2>&1 || cleanup_and_bail 121 "BIN Build failure"
+( mkdir tmp && touch tmp/eula_accepted )	|| cleanup_and_bail 121 "Unable to cheat eula for BIN package"
+( make ) >/dev/null 2>&1			|| cleanup_and_bail 121 "BIN Build failure"
+cd $TMPDIR
 
+# if we got this far, makes all ran ok. do the installs
+echo "Installing OSS"
+( cd $OSSDIR && make install ) >/dev/null 2>&1	|| cleanup_and_bail 120 "Failure to install OSS"
+cd $TMPDIR
+echo "Installing BIN"
+( cd $BINDIR && make install ) >/dev/null 2>&1	|| cleanup_and_bail 120 "Failure to install BIN"
+cd $TMPDIR
 
-
-echo "continuing..."
+echo "Done. Thank You!"
 cleanup_and_bail 0
 
